f9fb5ef726d0fb841bab5e555cfa91b4ce3b8f0c
Sergei W. Adonev <admin@cnlist.us>
2019-11-04 15:04:19 +0500

    servlet

--- src/main/java/us/cnlist/services/cityserv/CityservApplication.java
+++ src/main/java/us/cnlist/services/cityserv/CityservApplication.java
@@ -2,9 +2,16 @@ package us.cnlist.services.cityserv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"us.cnlist.services.cityserv"})
-public class CityservApplication {
+public class CityservApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CityservApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CityservApplication.class, args);
